a486ead6d5c3f6eaeba2c3924124ae2336076a4a
Devanshudd <devanshudd4@gmail.com>
2024-03-18 19:00:34 +0530

    changes done

--- src/components/Main.js
+++ src/components/Main.js
@@ -27,7 +27,7 @@ const Main = () => {
 
   const fetchData = async () => {
     try{
-      const data = await fetch('https://corsproxy.org/?' +  encodeURIComponent(Swiggy_URL));
+      const data = await fetch(Swiggy_URL);
 
       const json = await data.json();
 
